d684d6941389e5a3ea0f7e974fad5e7369eb4d46
Ramesha javed <165018589+Ramesha-sheikh@users.noreply.github.com>
2024-12-04 20:20:52 +0500

    first commit

---
+++ src/components/card.tsx
@@ -0,0 +1,246 @@
+import React from 'react';
+import Image from 'next/image'
+
+const Scard = () => {
+  return (
+    <>
+      {/* Responsive Grid Cards */}
+      <div className="w-full h-screen bg-white grid place-items-center 
+        grid-cols-2 xs:grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 
+        gap-4 sm:gap-6 md:gap-8 lg:gap-5  px-4 sm:px-6 md:px-10 lg:px-12">
+
+
+<div className="grid place-items-center w-24 h-24 xs:w-36 xs:h-60 sm:w-40 sm:h-72  md:w-48 md:h-[300px] lg:w-56 lg:h-80
+  shadow-md hover:shadow-xl transition-shadow duration-300 shadow-gray-400 bg-white rounded-lg">
+  <div className="w-full h-full    overflow-hidden grid  place-items-center ">
+  <Image
+      src="/sh.jpg"
+      alt="shoes"
+      width={200}
+      height={200}
+      className="object-cover px-2 rounded-md xs:h-24 sm:h-32 md:h-36 mt-5 lg:h-40 w-full"
+    />
+     <h1 className='bg-black text-white rounded-sm text-sm px-3'>Bigest Sale</h1>
+   <p className=' text-sm font-bold'>Up to 50% off</p>
+  
+ 
+  <h1 className='text-sm font-extrabold bg-gray-300 px-4 rounded-md'> Rs : 1,4500</h1>
+  <h2   className='text-lg font-bold  bg-pink-300 px-4 py-1 rounded-md text-black hover:bg-black hover:text-white transition-all duration-300'>Order Now</h2>
+  </div>
+</div>
+
+<div className="grid place-items-center w-24 h-24  xs:w-36 xs:h-60  sm:w-40 sm:h-72  md:w-48 md:h-[300px]   lg:w-56 lg:h-80
+  shadow-md hover:shadow-xl transition-shadow duration-300 shadow-gray-400 bg-white rounded-lg">
+  <div className="w-full h-full    overflow-hidden grid  place-items-center ">
+  <Image
+      src="/s1.jpg"
+      alt="shoes"
+      width={200}
+      height={200}
+      className="object-cover px-2 rounded-md xs:h-24 sm:h-32 md:h-36 mt-5 lg:h-40 w-full"
+    />
+
+<h1 className='bg-black text-white rounded-sm text-sm px-3'>Bigest Sale</h1>
+   <p className=' text-sm font-bold'>Up to 50% off</p>
+  
+ 
+  <h1 className='text-sm font-extrabold bg-gray-300 px-4 rounded-md'> Rs : 1,4500</h1>
+  <h2   className='text-lg font-bold  bg-pink-300 px-4 py-1 rounded-md text-black hover:bg-black hover:text-white transition-all duration-300'>Order Now</h2>
+  </div>
+</div>
+
+
+<div className="grid place-items-center w-24 h-24  xs:w-36 xs:h-60  sm:w-40 sm:h-72 md:w-48 md:h-[300px]    lg:w-56 lg:h-80
+  shadow-md hover:shadow-xl transition-shadow duration-300 shadow-gray-400 bg-white rounded-lg">
+  <div className="w-full h-full    overflow-hidden grid  place-items-center ">
+  <Image
+      src="/s13.jpg"
+      alt="shoes"
+      width={200}
+      height={200}
+      className="object-cover px-2 rounded-md xs:h-24 sm:h-32 md:h-36 mt-5 lg:h-40 w-full"
+    />
+
+<h1 className='bg-black text-white rounded-sm text-sm px-3'>Bigest Sale</h1>
+   <p className=' text-sm font-bold'>Up to 50% off</p>
+  
+ 
+  <h1 className='text-sm font-extrabold bg-gray-300 px-4 rounded-md'> Rs : 1,4500</h1>
+  <h2   className='text-lg font-bold  bg-pink-300 px-4 py-1 rounded-md text-black hover:bg-black hover:text-white transition-all duration-300'>Order Now</h2>
+  </div>
+</div>
+
+<div className="grid place-items-center w-24 h-24  xs:w-36 xs:h-60  sm:w-40 sm:h-72 md:w-48 md:h-[300px]   lg:w-56 lg:h-80
+  shadow-md hover:shadow-xl transition-shadow duration-300 shadow-gray-400 bg-white rounded-lg">
+  <div className="w-full h-full    overflow-hidden grid  place-items-center ">
+  <Image
+      src="/s3.jpg"
+      alt="shoes"
+      width={200}
+      height={200}
+      className="object-cover px-2 rounded-md xs:h-24 sm:h-32 md:h-36 mt-5 lg:h-40 w-full"
+    />
+
+<h1 className='bg-black text-white rounded-sm text-sm px-3'>Bigest Sale</h1>
+   <p className=' text-sm font-bold'>Up to 50% off</p>
+  
+ 
+  <h1 className='text-sm font-extrabold bg-gray-300 px-4 rounded-md'> Rs : 1,4500</h1>
+  <h2   className='text-lg font-bold  bg-pink-300 px-4 py-1 rounded-md text-black hover:bg-black hover:text-white transition-all duration-300'>Order Now</h2>
+  </div>
+</div>
+
+
+<div className="grid place-items-center w-24 h-24 xs:w-36 xs:h-60   sm:w-40 sm:h-72 md:w-48 md:h-[300px]   lg:w-56 lg:h-80
+  shadow-md hover:shadow-xl transition-shadow duration-300 shadow-gray-400 bg-white rounded-lg">
+  <div className="w-full h-full    overflow-hidden grid  place-items-center ">
+  <Image
+      src="/s4.jpg"
+      alt="shoes"
+      width={200}
+      height={200}
+      className="object-cover px-2 rounded-md xs:h-24 sm:h-32 md:h-36 mt-5 lg:h-40 w-full"
+    />
+     <h1 className='bg-black text-white rounded-sm text-sm px-3'>Bigest Sale</h1>
+   <p className=' text-sm font-bold'>Up to 50% off</p>
+  
+ 
+  <h1 className='text-sm font-extrabold bg-gray-300 px-4 rounded-md'> Rs : 1,4500</h1>
+  <h2   className='text-lg font-bold  bg-pink-300 px-4 py-1 rounded-md text-black hover:bg-black hover:text-white transition-all duration-300'>Order Now</h2>
+  </div>
+</div>
+
+
+<div className="grid place-items-center w-24 h-24  xs:w-36 xs:h-60  sm:h-72  sm:w-40 md:w-48 md:h-[300px]   lg:w-56 lg:h-80
+  shadow-md hover:shadow-xl transition-shadow duration-300 shadow-gray-400 bg-white rounded-lg">
+  <div className="w-full h-full    overflow-hidden grid  place-items-center ">
+  <Image
+      src="/s5.jpg"
+      alt="shoes"
+      width={200}
+      height={200}
+      className="object-cover px-2 rounded-md xs:h-24 sm:h-32  md:h-36 mt-5 lg:h-40 w-full"
+    />
+     <h1 className='bg-black text-white rounded-sm text-sm px-3'>Bigest Sale</h1>
+   <p className=' text-sm font-bold'>Up to 50% off</p>
+  
+ 
+  <h1 className='text-sm font-extrabold bg-gray-300 px-4 rounded-md'> Rs : 1,4500</h1>
+  <h2   className='text-lg font-bold  bg-pink-300 px-4 py-1 rounded-md text-black hover:bg-black hover:text-white transition-all duration-300'>Order Now</h2>
+  </div>
+</div>
+
+
+<div className="grid place-items-center w-24 h-24  xs:w-36 xs:h-60  sm:w-40 sm:h-72 md:w-48 md:h-[300px]   lg:w-56 lg:h-80
+  shadow-md hover:shadow-xl transition-shadow duration-300 shadow-gray-400 bg-white rounded-lg">
+  <div className="w-full h-full    overflow-hidden grid  place-items-center ">
+  <Image
+      src="/s12.jpg"
+      alt="shoes"
+      width={200}
+      height={200}
+      className="object-cover px-2 rounded-md xs:h-24 sm:h-32 md:h-36 mt-5 lg:h-40 w-full"
+    />
+     <h1 className='bg-black text-white rounded-sm text-sm px-3'>Bigest Sale</h1>
+   <p className=' text-sm font-bold'>Up to 50% off</p>
+  
+ 
+  <h1 className='text-sm font-extrabold bg-gray-300 px-4 rounded-md'> Rs : 1,4500</h1>
+  <h2   className='text-lg font-bold  bg-pink-300 px-4 py-1 rounded-md text-black hover:bg-black hover:text-white transition-all duration-300'>Order Now</h2>
+  </div>
+</div>
+
+
+<div className="grid place-items-center w-24 h-24  xs:w-36 xs:h-60  sm:w-40 sm:h-72 md:w-48 md:h-[300px]   lg:w-56 lg:h-80
+  shadow-md hover:shadow-xl transition-shadow duration-300 shadow-gray-400 bg-white rounded-lg">
+  <div className="w-full h-full    overflow-hidden grid  place-items-center ">
+  <Image
+      src="/s7.jpg"
+      alt="shoes"
+      width={200}
+      height={200}
+      className="object-cover px-2 rounded-md xs:h-24 sm:h-32 md:h-36 mt-5 lg:h-40 w-full"
+    />
+
+<h1 className='bg-black text-white rounded-sm text-sm px-3'>Bigest Sale</h1>
+   <p className=' text-sm font-bold'>Up to 50% off</p>
+  
+ 
+  <h1 className='text-sm font-extrabold bg-gray-300 px-4 rounded-md'> Rs : 1,4500</h1>
+  <h2   className='text-lg font-bold  bg-pink-300 px-4 py-1 rounded-md text-black hover:bg-black hover:text-white transition-all duration-300'>Order Now</h2>
+  </div>
+</div>
+
+
+<div className="grid place-items-center w-24 h-24  xs:w-36 xs:h-60 sm:w-40 sm:h-72 md:w-48 md:h-[300px]   lg:w-56 lg:h-80
+  shadow-md hover:shadow-xl transition-shadow duration-300 shadow-gray-400 bg-white rounded-lg ">
+  <div className="w-full h-full    overflow-hidden grid  place-items-center ">
+    <Image
+      src="/s8.jpg"
+      alt="shoes"
+      width={200}
+      height={200}
+      className="object-cover px-2 rounded-md xs:h-24 sm:h-32 md:h-36 mt-5 lg:h-40 lg:w-full"
+    />
+   <h1 className='bg-black text-white rounded-sm text-sm px-3'>Bigest Sale</h1>
+   <p className=' text-sm font-bold'>Up to 50% off</p>
+  
+ 
+  <h1 className='text-sm font-extrabold bg-gray-300 px-4 rounded-md'> Rs : 1,4500</h1>
+  <h2   className='text-lg font-bold  bg-pink-300 px-4 py-1 rounded-md text-black hover:bg-black hover:text-white transition-all duration-300'>Order Now</h2>
+
+  </div>
+</div>
+
+
+
+<div className="grid place-items-center w-24 h-24  xs:w-36 xs:h-60  sm:w-40 sm:h-72 md:w-48 md:h-[300px]   lg:w-56 lg:h-80
+  shadow-md hover:shadow-xl transition-shadow duration-300 shadow-gray-400 bg-white rounded-lg">
+  <div className="w-full h-full    overflow-hidden grid  place-items-center ">
+  <Image
+      src="/s9.jpg"
+      alt="shoes"
+      width={200}
+      height={200}
+      className="object-cover px-2 rounded-md xs:h-24 sm:h-32 md:h-36 mt-5 lg:h-40 w-full"
+    />
+     <h1 className='bg-black text-white rounded-sm text-sm px-3'>Bigest Sale</h1>
+   <p className=' text-sm font-bold'>Up to 50% off</p>
+  
+ 
+  <h1 className='text-sm font-extrabold bg-gray-300 px-4 rounded-md'> Rs : 1,4500</h1>
+  <h2   className='text-lg font-bold  bg-pink-300 px-4 py-1 rounded-md text-black hover:bg-black hover:text-white transition-all duration-300'>Order Now</h2>
+  </div>
+</div>
+
+<div className="grid place-items-center w-24 h-24  xs:w-36 xs:h-60  sm:w-40 sm:h-72 md:w-48 md:h-[300px]  lg:w-56 lg:h-80
+  shadow-md hover:shadow-xl transition-shadow duration-300 shadow-gray-400 bg-white rounded-lg">
+  <div className="w-full h-full    overflow-hidden grid  place-items-center ">
+  <Image
+      src="/s10.jpg"
+      alt="shoes"
+      width={200}
+      height={200}
+      className="object-cover px-2 rounded-md xs:h-24 sm:h-32 md:h-36 mt-5 lg:h-40 w-full"
+    />
+     <h1 className='bg-black text-white rounded-sm text-sm px-3'>Bigest Sale</h1>
+   <p className=' text-sm font-bold'>Up to 50% off</p>
+  
+ 
+  <h1 className='text-sm font-extrabold bg-gray-300 px-4 rounded-md'> Rs : 1,4500</h1>
+  <h2   className='text-lg font-bold  bg-pink-300 px-4 py-1 rounded-md text-black hover:bg-black hover:text-white transition-all duration-300'>Order Now</h2>
+  </div>
+</div>
+
+         
+
+
+
+       
+
+      </div>
+    </>
+  );
+};
+
+export default Scard;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,23 @@
-import type { Config } from "tailwindcss";
-
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      screens: {
+        'xs': '320px', // Custom for very small screens
+        'sm': '480px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
       },
     },
   },
   plugins: [],
-} satisfies Config;
+};
